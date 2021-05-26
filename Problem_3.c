7b4020e71e80ae7464f5fcf86d652e14f5e5daf0
RNischitha <>
2021-05-26 11:40:24 +0000

    Odd/even

--- Problem_3.c
+++ Problem_3.c
@@ -7,7 +7,14 @@ int main() {
     scanf("%d", &number);
 
     // Write your code here
-    
+    if(number%2==0)
+    {
+      printf("%d is an even number", number);
+    }
+    else
+    {
+      printf("%d is an old number", number);
+    }
     
    
     return 0;
